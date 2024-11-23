1615976cda29ea7390154ad7dba8fe8e41c14986
Laura <laura_habdul@hotmail.fr>
2024-11-23 17:14:45 +0100

    refactor: refactor CalculateActivityPrice to use strategy pattern for selecting and delegating pricing calculation

--- domain/src/main/java/com/activities/pricing/domain/usecases/CalculateActivityPrice.java
+++ domain/src/main/java/com/activities/pricing/domain/usecases/CalculateActivityPrice.java
@@ -7,42 +7,23 @@ import java.time.DayOfWeek;
 
 public class CalculateActivityPrice {
 
-    public double calculateFreeSaleActivityPrice(Activity activity, int adults, int children, DayOfWeek day) {
+    private final ActivityPriceCalculatorManager activityPriceCalculator;
 
-        checkActivityAvailability(activity, day);
-
-        double price = activity.getAdultPrice() * adults;
-
-        if (children < 2) {
-            // If less than 2, then just child number * price
-            price += activity.getChildPrice() * children;
-        } else {
-            // If greater than 2, then 2 * price
-            price += activity.getChildPrice() * 2;
-            // And the rest of the children reduced rate of price / 2
-            price += (activity.getChildPrice() * (children - 2)) / 2;
-        }
-
-        return price;
+    public CalculateActivityPrice(ActivityPriceCalculatorManager activityPriceCalculator) {
+        this.activityPriceCalculator = activityPriceCalculator;
     }
 
-    public double calculateOnDemandActivityPrice(Activity activity, int adults, int children, DayOfWeek day) {
-
-        checkActivityAvailability(activity, day);
-
-        int total = adults + children;
-
-        if (total > 4) {
-            throw new InvalidPricingRequest("Places are limit to 4");
-        }
-
-        return (adults * activity.getAdultPrice()) + (children * activity.getChildPrice());
-    }
-
-    private void checkActivityAvailability(Activity activity, DayOfWeek day) {
+    public double calculateActivityPrice(Activity activity, int adults, int children, DayOfWeek day) {
 
         if (!activity.getOpenDays().contains(day)) {
-            throw new InvalidPricingRequest("Activity not available on %s".formatted(day));
+            throw new InvalidPricingRequest(String.format("Activity '%s' is not available on %s.", activity.getTitle(), day));
         }
+        PriceCalculator calculator = activityPriceCalculator.getCalculator(activity.getCode());
+
+        if (calculator == null) {
+            throw new InvalidPricingRequest("No calculator found for code: " + activity.getCode());
+        }
+
+        return calculator.calculateActivityPrice(activity, adults, children, day);
     }
 }
